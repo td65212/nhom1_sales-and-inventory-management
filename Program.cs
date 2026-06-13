2b3773c53276c55db93cc4b956f02628cb2748f8
huyen64 <162551918+vminhquan@users.noreply.github.com>
2026-06-13 12:51:48 +0700

    x

--- Program.cs
+++ Program.cs
@@ -88,6 +88,9 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://localhost:4173",
+                "http://127.0.0.1:4173",
                 "https://front-end-sales-and-inventory-management.onrender.com")
             .AllowAnyHeader()
             .AllowAnyMethod();
